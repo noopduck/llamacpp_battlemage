a01faf95462df57cd13d4ca4492be18d721df74d
Rahmi Acar <rahmiac@protonmail.com>
2026-06-13 01:54:28 +0200

    Changed directory of model to rahmi

--- run_llama_sycl_gemma4-B36-A4B.sh
+++ run_llama_sycl_gemma4-B36-A4B.sh
@@ -3,7 +3,7 @@
 source /opt/intel/oneapi/setvars.sh --force
 
 bin/llama-server \
-  -m /home/rahmi/.cache/huggingface/hub/models--unsloth--gemma-4-26B-A4B-it-GGUF/snapshots/3bb10d594514ef4edb7f3a65d41a7e4eb8c5767a/gemma-4-26B-A4B-it-UD-Q4_K_M.gguf \
+  -m /home/$USER/.cache/huggingface/hub/models--unsloth--gemma-4-26B-A4B-it-GGUF/snapshots/3bb10d594514ef4edb7f3a65d41a7e4eb8c5767a/gemma-4-26B-A4B-it-UD-Q4_K_M.gguf \
   -ngl 99 \
   --flash-attn on \
   --host 0.0.0.0 \
