bbdc8ce30711949f34115ea5813c31fba948da16
Noopduck <rahmiac@protonmail.com>
2026-06-15 23:12:12 +0200

    Enable OpenSSL support in build script

--- build_llamacpp_sycl.sh
+++ build_llamacpp_sycl.sh
@@ -4,8 +4,8 @@ source /opt/intel/oneapi/setvars.sh
 
 cmake . \
     -DLLAMA_SYCL=ON \
+    -DLLAMA_OPENSSL=ON \
     -DCMAKE_C_COMPILER=icx \
     -DCMAKE_CXX_COMPILER=icpx \
     -DLLAMA_SYCL_F16=ON
 make -j$(nproc)
-
