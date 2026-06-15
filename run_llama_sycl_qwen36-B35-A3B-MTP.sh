8007e14b2d4e1f445626f6d251f48190071d58fc
Noopduck <rahmiac@protonmail.com>
2026-06-15 23:19:32 +0200

    Increase batch sizes for llama server

--- run_llama_sycl_qwen36-B35-A3B-MTP.sh
+++ run_llama_sycl_qwen36-B35-A3B-MTP.sh
@@ -6,6 +6,8 @@ bin/llama-server \
   -m /home/$USER/.cache/huggingface/hub/models--unsloth--Qwen3.6-35B-A3B-GGUF/snapshots/a483e9e6cbd595906af30beda3187c2663a1118c/Qwen3.6-35B-A3B-UD-Q4_K_M.gguf \
   -ngl 99 \
   --flash-attn on \
+  --batch-size 4096 \
+  --ubatch-size 1024 \
   --host 0.0.0.0 \
   --port 11434
 
