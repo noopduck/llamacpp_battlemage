7fa74031a90458a7dd084a4d813562d3fe0f3f61
Noopduck <rahmiac@protonmail.com>
2026-06-15 23:19:52 +0200

    Increase batch sizes for llama server execution

--- run_llama_sycl_gemma4-B36-A4B.sh
+++ run_llama_sycl_gemma4-B36-A4B.sh
@@ -6,6 +6,8 @@ bin/llama-server \
   -m /home/$USER/.cache/huggingface/hub/models--unsloth--gemma-4-26B-A4B-it-GGUF/snapshots/3bb10d594514ef4edb7f3a65d41a7e4eb8c5767a/gemma-4-26B-A4B-it-UD-Q4_K_M.gguf \
   -ngl 99 \
   --flash-attn on \
+  --batch-size 4096 \
+  --ubatch-size 1024 \
   --host 0.0.0.0 \
   --port 11434
 
