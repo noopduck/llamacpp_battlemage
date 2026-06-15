f6aa8d7d6d71414b41cefa9e099ddd54849ad54c
Noopduck <rahmiac@protonmail.com>
2026-06-15 23:23:44 +0200

    Update llama-server model path in script

--- run_llama_sycl_qwen36-B35-A3B-MTP.sh
+++ run_llama_sycl_qwen36-B35-A3B-MTP.sh
@@ -3,7 +3,7 @@
 source /opt/intel/oneapi/setvars.sh --force
 
 bin/llama-server \
-  -m /home/$USER/.cache/huggingface/hub/models--unsloth--Qwen3.6-35B-A3B-GGUF/snapshots/a483e9e6cbd595906af30beda3187c2663a1118c/Qwen3.6-35B-A3B-UD-Q4_K_M.gguf \
+  -hf unsloth/Qwen3.6-35B-A3B-MTP-GGUF:UD-Q4_K_M \
   -ngl 99 \
   --flash-attn on \
   --batch-size 4096 \
