a3aaead18df8820d7f80d9313af6d020e3ea474d
Noopduck <rahmiac@protonmail.com>
2026-06-15 23:23:07 +0200

    Update model paths in run_llama_sycl_gemma4 script

--- run_llama_sycl_gemma4-B36-A4B.sh
+++ run_llama_sycl_gemma4-B36-A4B.sh
@@ -3,7 +3,7 @@
 source /opt/intel/oneapi/setvars.sh --force
 
 bin/llama-server \
-  -m /home/$USER/.cache/huggingface/hub/models--unsloth--gemma-4-26B-A4B-it-GGUF/snapshots/3bb10d594514ef4edb7f3a65d41a7e4eb8c5767a/gemma-4-26B-A4B-it-UD-Q4_K_M.gguf \
+  -hf unsloth/gemma-4-26B-A4B-it-qat-GGUF:UD-Q4_K_XL \
   -ngl 99 \
   --flash-attn on \
   --batch-size 4096 \
